2f8089848c617c6f3eb45228e58695984135d509
mouni <naremmounika@gmail.com>
2026-09-13 16:03:37 +0530

    Fix Vercel CORS origin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://reconciliation-dashboard-kq54.vercel.app/",
+    "https://reconciliation-dashboard-kq54.vercel.app",
 ]
